f59cfdb86be4b38341fb6a06a2594b05f2605ddb
Ajesh <ajesh@prixa.org>
2018-10-11 13:22:55 +0545

    Exclude __all__ and datetime field supported

--- testapp/models.py
+++ testapp/models.py
@@ -23,12 +23,24 @@ class Tag(DateTimeModel):
 	def __str__(self):
 		return self.name
 
+
+class Author(DateTimeModel):
+	name = models.CharField(max_length=255, unique=True)
+
+	def __str__(self):
+		return self.name
+
+
 class Subject(DateTimeModel):
 	# Main model to be import/export
 	class_obj = models.ForeignKey(Class, related_name='subjects', on_delete=models.CASCADE)
+	author = models.OneToOneField(Author, null=True, blank=True)
 	code = models.CharField(max_length=255, unique=True)
 	name = models.CharField(max_length=255, unique=True)
 	tags = models.ManyToManyField(Tag, related_name='subjects')
+	date = models.DateField(null=True, blank=True)
+	time = models.TimeField(null=True, blank=True)
+	datetime = models.DateTimeField(null=True, blank=True)
 
 	def __str__(self):
 		return self.name
--- testapp/resources.py
+++ testapp/resources.py
@@ -1,12 +1,15 @@
-from model_import_export.resources import ModelResource, ForeignKeyResource, ManyToManyResource, RelatedResource
+from model_import_export.resources import *
 
 from .models import *
 
 class SubjectResource(ModelResource):
 	class_obj = ForeignKeyResource(column='name')
+	author = OneToOneResource(column='name')
 	tags = ManyToManyResource(column='name')
 	books = RelatedResource(column='name')
 
 	class Meta:
 		model = Subject
-		fields = ['class_obj', 'code', 'name', 'tags', 'books']
+		fields = ['class_obj', 'author', 'code', 'name', 'tags', 'books', 'date' , 'time', 'datetime']
+		# fields = '__all__'
+		# exclude = ['date', 'tags']
